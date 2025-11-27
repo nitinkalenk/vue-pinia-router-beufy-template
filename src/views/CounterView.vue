d4abf2f93a67d73b7affce6aa4c2758e5a5a54b6
nkale <nkale@medline.com>
2025-11-27 18:11:14 +0530

    added watcher

--- src/views/CounterView.vue
+++ src/views/CounterView.vue
@@ -1,12 +1,17 @@
 <script setup lang="ts">
 import { BButton } from "buefy";
-import { ref } from "vue";
+import { ref, watch } from "vue";
 
 const counter = ref(0);
 
 function increment() {
   counter.value++;
 }
+
+watch(counter, (newCount) => {
+  // yes, console.log() is a side effect
+  console.log(`new count is: ${newCount}`)
+})
 
 </script>
 
