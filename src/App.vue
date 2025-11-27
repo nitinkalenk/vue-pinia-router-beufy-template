306f73d3d462a16757d4845c64fba6a578a67242
nkale <nkale@medline.com>
2025-11-27 13:22:52 +0530

    added buefy

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
 import HelloWorld from './components/HelloWorld.vue'
+import { BButton } from "buefy";
 </script>
 
 <template>
   <header>
+    <b-button type="is-primary">Primary</b-button>
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,17 @@
 import './assets/main.css'
+import 'buefy/dist/css/buefy.css'
 
 import { createApp } from 'vue'
-import { createPinia } from 'pinia'
-
 import App from './App.vue'
+
+import { createPinia } from 'pinia'
 import router from './router'
+import Buefy from 'buefy'
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(Buefy)
 
 app.mount('#app')
